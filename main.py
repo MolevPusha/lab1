ea056dae1b867b7a9649ad73a1b4320b054c744e
Pavel Molev <onvon5404@gmail.com>
2024-12-06 14:28:43 +0300

    task5

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 # task 1
-#
-# array = [1, 2, 19, 120, -9999, 232, 2]
-#
-# maxInt = array[0]
-# minInt = array[0]
-#
-# for num in array:
-#     if num > maxInt:
-#         maxInt = num
-#     if num < minInt:
-#         minInt = num
-#
-# print(maxInt - minInt)
+
+array = [1, 2, 19, 120, -9999, 232, 2]
+
+maxInt = array[0]
+minInt = array[0]
+
+for num in array:
+    if num > maxInt:
+        maxInt = num
+    if num < minInt:
+        minInt = num
+
+print(maxInt - minInt)
 
 
 # task 2
-import random
-
-arrayLength = 10
-array = []
-
-for _ in range(arrayLength):
-    randomNumber = int((round(random.random(), 2)) * 200) - 100
-    array.append(randomNumber)
-print(array)
+# import random
+#
+# arrayLength = 10
+# array = []
+#
+# for _ in range(arrayLength):
+#     randomNumber = int((round(random.random(), 2)) * 200) - 100
+#     array.append(randomNumber)
+# print(array)
 
 # task 3
 # import random
